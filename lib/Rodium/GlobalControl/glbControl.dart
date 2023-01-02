429d099f4f4be09827803c92ec4ad45ee146572a
TepDe <unsokontep.swdev@gmail.com>
2023-01-02 10:38:26 +0700

    Create glbControl.dart
    home screen

---
+++ lib/Rodium/GlobalControl/glbControl.dart
@@ -0,0 +1,10 @@
+
+class StrKey {
+  String testSave = 'testSave';
+  String saveTitleList = 'saveTitleList';
+  String saveTitleListSec = 'saveTitleListSec';
+  String saveTitleLisMerge = 'saveTitleLisMerge';
+  String saveSubtitleList = 'saveSubtitleList';
+  String saveSubtitleListSec = 'saveSubtitleListSec';
+  String objPlaylist = 'objPlaylist';
+}
